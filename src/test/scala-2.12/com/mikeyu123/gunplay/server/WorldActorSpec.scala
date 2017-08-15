32bb9a1c34d7d273ca9268791f88f2cebddf1e3e
Mike Yurchenkov <mikesehse@gmail.com>
2017-08-15 15:06:14 +0300

    Add more tests

--- src/test/scala-2.12/com/mikeyu123/gunplay/server/WorldActorSpec.scala
+++ src/test/scala-2.12/com/mikeyu123/gunplay/server/WorldActorSpec.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import com.mikeyu123.gunplay.TestUtils
 import com.mikeyu123.gunplay.objects.Body
+import com.mikeyu123.gunplay_physics.structs.Vector
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 /**
@@ -20,10 +21,25 @@ class WorldActorSpec extends TestKit(ActorSystem()) with ImplicitSender with
   val schedulerProbe = TestProbe()
   val actorRef = TestActorRef(new WorldActor())
   val actor: WorldActor = actorRef.underlyingActor
+  val sampleBody: Body = Body.initBody(dummyUuid, 50, -50)
 
   it should "add player correctly" in {
     actorRef ! AddPlayer(dummyUuid, 50, -50)
-    val body = Body.initBody(dummyUuid, 50, -50)
-    actor.world.players should contain (body)
+    actor.world.players should contain (sampleBody)
+  }
+
+  it should "update player correctly" in {
+    actorRef ! AddPlayer(dummyUuid, 50, -50)
+    actorRef ! UpdateControls(Vector(-1, 1), .5)
+    val newBody = Body(sampleBody.uuid, sampleBody.graphicsObject, Vector(-1, 1), .5)
+    actor.world.players should contain (newBody)
+  }
+
+  it should "update step correctly" in {
+    actorRef ! AddPlayer(dummyUuid, 50, -50)
+    actorRef ! UpdateControls(Vector(-1, 1), .5)
+    schedulerProbe.send(actorRef, Step)
+    val newBody = Body(sampleBody.uuid, sampleBody.graphicsObject.move(Vector(-1, 1)), Vector(-1, 1), .5)
+    actor.world.players should contain (newBody)
   }
 }

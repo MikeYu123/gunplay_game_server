4c7234f4a5fe9e43c97e65a5166b21a154256084
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2019-01-28 18:25:21 +0300

    Rework shotgun

--- src/main/scala-2.12/com/mikeyu123/gunplay/objects/huy/Shotgun.scala
+++ src/main/scala-2.12/com/mikeyu123/gunplay/objects/huy/Shotgun.scala
@@ -21,44 +21,19 @@ case class Shotgun(span: Long = Shotgun.span, bulletVelocity: Double = Shotgun.b
 
   def emit(player: Player): Set[Bullet] = {
     if(lastFired.plus(span, ChronoUnit.MILLIS).isBefore(Instant.now) && ammo > 0) {
-      val bullet1 = new Bullet(player.getId, position =
-        player.getWorldCenter.add(
-          Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
-        velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation)
-      )
-      val bullet2 = new Bullet(player.getId, position =
-        player.getWorldCenter.add(
-          Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
-        velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation - Math.PI / 6)
-      )
-      val bullet3 = new Bullet(player.getId, position =
-        player.getWorldCenter.add(
-          Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
-        velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation + Math.PI / 6)
-      )
-      val bullet4 = new Bullet(player.getId, position =
-        player.getWorldCenter.add(
-          Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
-        velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation - Math.PI / 12)
-      )
-      val bullet5 = new Bullet(player.getId, position =
-        player.getWorldCenter.add(
-          Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
-        velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation + Math.PI / 12)
-      )
-      bullet1.getTransform.setRotation(player.getTransform.getRotation)
-      bullet1.setAsleep(false)
-      bullet2.getTransform.setRotation(player.getTransform.getRotation - Math.PI / 6)
-      bullet2.setAsleep(false)
-      bullet3.getTransform.setRotation(player.getTransform.getRotation + Math.PI / 6)
-      bullet3.setAsleep(false)
-      bullet4.getTransform.setRotation(player.getTransform.getRotation - Math.PI / 12)
-      bullet4.setAsleep(false)
-      bullet5.getTransform.setRotation(player.getTransform.getRotation + Math.PI / 12)
-      bullet5.setAsleep(false)
+      val bullets = Set(-Math.PI/6, -Math.PI / 3, 0d, Math.PI/6, Math.PI / 3).map(offset => {
+        val bullet = new Bullet(player.getId, position =
+          player.getWorldCenter.add(
+            Vector2(Shotgun.bulletOffset, 0).rotate(player.getTransform.getRotation)),
+          velocity = Vector2(bulletVelocity, 0).rotate(player.getTransform.getRotation + offset)
+        )
+        bullet.getTransform.setRotation(player.getTransform.getRotation + offset)
+        bullet.setAsleep(false)
+        bullet
+      })
       lastFired = Instant.now
       ammo -= 1
-      Set(bullet1, bullet2, bullet3, bullet4, bullet5)
+      bullets
     }
     else {
       Set()

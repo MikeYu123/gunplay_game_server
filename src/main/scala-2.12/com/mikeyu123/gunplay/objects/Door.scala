812b8949830ddede7edb7a4a5a00b9c54678bc94
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2019-01-31 12:12:23 +0300

    Fix some

--- src/main/scala-2.12/com/mikeyu123/gunplay/objects/Door.scala
+++ src/main/scala-2.12/com/mikeyu123/gunplay/objects/Door.scala
@@ -3,7 +3,7 @@ package com.mikeyu123.gunplay.objects
 import java.util.UUID
 
 import org.dyn4j.geometry.{Geometry, MassType}
-import Door.{Pin, defaultHeight, defaultWidth}
+import Door.{Pin, defaultHeight, defaultWidth, defaultDamping}
 import com.mikeyu123.gunplay.utils
 import com.mikeyu123.gunplay.utils.LevelParser.PinData
 import com.mikeyu123.gunplay.utils.Vector2
@@ -13,6 +13,7 @@ import org.dyn4j.dynamics.joint.RevoluteJoint
 object Door {
   val defaultHeight = utils.AppConfig.getInt("door.height")
   val defaultWidth = utils.AppConfig.getInt("door.width")
+  val defaultDamping = utils.AppConfig.getDouble("door.damping")
   case class Pin(x: Double, y: Double, width: Int, height: Int) extends Body() {
     addFixture(Geometry.createRectangle(width, height))
     setLinearVelocity(0,0)
@@ -27,12 +28,13 @@ class Door(width: Double = defaultWidth,
            height: Double = defaultHeight,
            position: Vector2 = Vector2(0, 0),
            velocity: Vector2 = Vector2(0, 0),
+           damping: Double = defaultDamping,
            val pin: Pin) extends Body{
   addFixture(Geometry.createRectangle(width, height))
   setLinearVelocity(velocity)
   translate(position)
   setAngularVelocity(0.0)
-  setAngularDamping(.3)
+  setAngularDamping(damping)
   setMass(MassType.NORMAL)
   val joint = new RevoluteJoint(this, pin, Vector2(pin.x, pin.y))
   joint.setLimitEnabled(true)
